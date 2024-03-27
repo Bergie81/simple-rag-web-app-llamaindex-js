ec75a45ed8c26e4237b9775e8ffbfc164d81a6f1
Thomas Bergmann <bergie81@gmail.com>
2024-03-28 00:05:40 +0100

    Detailed RAG

--- basic-rag.js
+++ basic-rag.js
@@ -1,17 +1,20 @@
-import "dotenv/config";
-// console.log(process.env.OPENAI_API_KEY);
+// Basic RAG with convenient functions
 import { Document, VectorStoreIndex, SimpleDirectoryReader } from "llamaindex";
+import "dotenv/config";
 
+// Load our data from a local directory
 const documents = await new SimpleDirectoryReader().loadData({
 	directoryPath: "./data",
 });
 
+// Initialize an index
 const index = await VectorStoreIndex.fromDocuments(documents);
 
+// Create a query engine: convenience function combines several components: Retriever, Postprocessing, Synthesizer
 const queryEngine = index.asQueryEngine();
 
+// Ask a question
 const response = await queryEngine.query({
-	query: "What is the product number of the Salmonella LyoKit?",
+	query: "How many cycles are run for the amplification in the program setup?",
 });
-
 console.log(response.toString());
